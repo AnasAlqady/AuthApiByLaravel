6bbad861b94a0b59bfcb1df673faf41f69f54007
AnasAlqady <anasalqady9@gmail.com>
2023-03-06 18:54:19 +0300

    adding all file

---
+++ app/Http/Controllers/Api/JWTAuthController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request; 
+use JWTAuth;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+
+class JWTAuthController extends Controller
+{
+    public $token = true;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+  
+    public function register(Request $request)
+    {
+        // return response()->json(['request'=>$request->name]);
+         $validator = Validator::make($request->all(),[ 
+                      'name' => 'required',
+                      'email' => 'required|string|email|unique:users',
+                      'password' => 'required|confirmed|min:6',   
+                      'phoneNo' => 'required|integer',
+                      'image' => 'required|file'
+                     ]);  
+         if ($validator->fails()) {  
+               return response()->json($validator->errors()->toJson(), 401 ); 
+         }   
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->phoneNo = $request->phoneNo;
+        $profileImage = $request->file('image');
+        $profileImageSaveAsName = time() . "-profile." . $profileImage->getClientOriginalExtension();
+        $upload_path = 'profile_images/';
+        $profile_image_url = $upload_path . $profileImageSaveAsName;
+        $success = $profileImage->move($upload_path, $profileImageSaveAsName);
+        $user->image =  $profile_image_url;
+        $user->save();
+        if ($this->token) {
+            return $this->login($request);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $user
+        ], Response::HTTP_OK);
+    }
+  
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $jwt_token = null;
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        return response()->json([
+            'success' => true,
+            'token' => $jwt_token,
+        ]);
+    }
+  
+    public function logout()
+    {
+
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+        // $this->validate($request, [
+        //     'token' => 'required'
+        // ]);
+  
+        // try {
+        //     JWTAuth::invalidate($request->token);
+  
+        //     return response()->json([
+        //         'success' => true,
+        //         'message' => 'User logged out successfully'
+        //     ]);
+        // } catch (JWTException $exception) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Sorry, the user cannot be logged out'
+        //     ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        // }
+    }
+  
+    public function getUser(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+  
+        $user = JWTAuth::authenticate($request->token);
+  
+        return response()->json(['user' => $user]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\JWTAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    die(1);
+});
+Route::group(['middleware'=>'api','prefix'=>'auth'],function($router){
+    Route::post('/register', [JWTAuthController::class, 'register']);
+    Route::post('/login', [JWTAuthController::class, 'login']);
+    Route::get('/logout', [JWTAuthController::class, 'logout']);
+});
+
+  
+// Route::group(['middleware' => 'auth.jwt'], function () {
+    
+// });
